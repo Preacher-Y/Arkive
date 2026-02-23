221ccf6d6a4a5db030a1ffc7524d869144686c6e
Preacher-Y <ysheja@gmail.com>
2026-02-23 14:28:35 +0200

    Added the User entity

---
+++ backend/src/main/java/com/arkive/backend/model/User.java
@@ -0,0 +1,36 @@
+package com.arkive.backend.model;
+
+import java.util.UUID;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "user_name", nullable = false)
+    @Size(max = 50)
+    private String name;
+
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Size(min=8)
+    private String password;
+
+}
