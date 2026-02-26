7ad3e81b1f3cf70046f8f94a8f5794a62b4ee81e
Preacher-Y <ysheja@gmail.com>
2026-02-26 14:36:58 +0200

    Added the security dependency for hashing the password

---
+++ backend/src/main/java/com/arkive/backend/util/SecurityConfi.java
@@ -0,0 +1,26 @@
+package com.arkive.backend.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfi {
+    
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder(16);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+            .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+}
