2fca583f823a3ccce32f7a431b4163d10bbfbac3
Preacher-Y <ysheja@gmail.com>
2026-02-27 12:02:22 +0200

    modified the security to permit only auth endpoints

--- backend/src/main/java/com/arkive/backend/util/SecurityConfi.java
+++ backend/src/main/java/com/arkive/backend/util/SecurityConfi.java
@@ -1,8 +1,9 @@
 package com.arkive.backend.util;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,8 +19,10 @@ public class SecurityConfi {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-            .csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                                    .anyRequest().authenticated())
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
